00cceef278fd47628b404b0f8760d63eb8f30f16
Nagamani <sikhakollilakshmi@iitism.ac.in>
2024-10-24 10:53:41 +0530

    Square and Cube implimented

--- calculator.c
+++ calculator.c
@@ -64,12 +64,12 @@ int main() {
             case '^':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("square of %d is %d\n",a,square(a));
                 break;
             case '#':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("cube of %d is %d\n",a,cube(a));
                 break;
             case 'i':
                 printf("Enter an integer: ");
